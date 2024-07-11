7d1f3512704c237506a1d8ee8bdf3c30ac1c1f55
FacuMaletta <47653928@est.ort.edu.ar>
2024-07-11 14:25:02 -0300

    Tp 3 subido

---
+++ Tp_dap_3/lib/core/data/empleados_datasource.dart
@@ -0,0 +1,96 @@
+
+
+
+import 'package:clase18_4/entities/Empleado.dart';
+import 'package:flutter/material.dart';
+
+final empleadolist = [
+    Empleado(
+      nombre:'Ale' , 
+      apellido:'Profe', 
+      area: 'IT', 
+      id: '1',
+      poster: 'https://st3.depositphotos.com/1177973/13569/i/450/depositphotos_135693246-stock-photo-male-teacher-in-classroom.jpg' ,
+    ),
+
+    Empleado(
+      nombre:'Mia' , 
+      apellido:'Payssé' , 
+      area: 'Operaciones', 
+      id: '2',
+      poster: 'https://media.licdn.com/dms/image/D4D03AQEEASd6Fbn9Ug/profile-displayphoto-shrink_200_200/0/1702344396909?e=2147483647&v=beta&t=4HttxvO--yhIVDuY6QIJmXGTUvgsoWMz0BDNTf9sivI' , 
+    ),
+
+    Empleado(
+      nombre:'Facundo' ,
+      apellido:'Maletta' ,
+      area: 'Operaciones', 
+      id: '3', 
+      poster: 'https://pbs.twimg.com/profile_images/1102999529622786048/qNH7_Bbo_400x400.jpg' , 
+    ),
+
+    Empleado(
+      nombre:'Natán' ,
+      apellido:'Rosenhain' ,
+      area: 'Finanzas', 
+      id: '4', 
+      poster: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg' , 
+    ),
+
+    Empleado(
+      nombre:'Juan Cruz' ,
+      apellido:'Rodriguez' ,
+      area: 'Ventas', 
+      id: '5', 
+      poster: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg' , 
+    ),
+    
+    Empleado(
+      nombre:'Ramiro' ,
+      apellido:'Lowden' ,
+      area: 'Contaduría', 
+      id: '5', 
+      poster: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg' , 
+    ),
+
+    Empleado(
+      nombre:'Santino' ,
+      apellido:'DePascale' ,
+      area: 'Administración', 
+      id: '6', 
+      poster: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg' , 
+    ),
+
+    Empleado(
+      nombre:'Feliciano' ,
+      apellido:'Miguez' ,
+      area: 'Marketing', 
+      id: '7', 
+      poster: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg' , 
+    ),
+
+    Empleado(
+      nombre:'Uriel' ,
+      apellido:'Dyner' ,
+      area: 'Ventas', 
+      id: '8', 
+      poster: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg' , 
+    ),
+
+    Empleado(
+      nombre:'Julián' ,
+      apellido:'Vinocur' ,
+      area: 'IT', 
+      id: '9', 
+      poster: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg' , 
+    ),
+
+    Empleado(
+      nombre:'Felipe' ,
+      apellido:'Sala' ,
+      area: 'IT', 
+      id: '10', 
+      poster: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg' , 
+    ),
+];
+
---
+++ Tp_dap_3/lib/presentation/Screens/infoempleados_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:ffi';
+
+import 'package:clase18_4/core/data/empleados_datasource.dart';
+import 'package:clase18_4/entities/Empleado.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class InfoempleadosScreen extends StatelessWidget {
+static const String name = 'infoempleados';
+
+ final String empleadoId;
+
+  const InfoempleadosScreen({super.key,  required this.empleadoId});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    final empleado = empleadolist.firstWhere((empleado) => empleado.id == empleadoId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalles del empleado'),
+      ),
+      body: _EmpleadoDetailView(empleado: empleado),
+    );
+  }
+}
+
+class _EmpleadoDetailView extends StatelessWidget {
+  const _EmpleadoDetailView({
+    super.key,
+    required this.empleado,
+  });
+
+  final Empleado empleado;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          if (empleado.poster != null) Image.network(empleado.poster!, height: 400),
+          const SizedBox(height: 16),
+          Text(
+            "${empleado.nombre} ${empleado.apellido}",
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Text(
+            'Area de trabajo: ${empleado.area}',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
